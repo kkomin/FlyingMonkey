93cc90e1967629c26c64ff056723481e24007dd1
suyeno <realsu_yeonini418@naver.com>
2023-07-10 21:41:34 +0900

    초안 3장

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,8 +13,7 @@ import 'package:monkeyplace/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
+    await tester.pumpWidget(Myapp());
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
