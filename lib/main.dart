5425b0f43e024113a672065af68b7b54089bea55
kkomin <100822055+kkomin@users.noreply.github.com>
2023-07-10 22:44:15 +0900

    first work

--- lib/main.dart
+++ lib/main.dart
@@ -38,45 +38,71 @@ class ThirdPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-            child: Row(
-              children: [
-                Align(
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  DefaultTabController.of(context).animateTo(0);
+                },
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.arrow_back_ios_new_outlined,
+                      color: Colors.grey[700],
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                child: Align(
                   alignment: Alignment.centerLeft,
                   child: ClipOval(
                     child: Container(
                       width: 100,
                       height: 100,
-                      color: Colors.blue,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                                "https://kkomin.github.io/dog.jpg")),
+                      ),
                       // 여기에 동그라미 모양의 사진 위젯을 추가하세요
                     ),
                   ),
                 ),
-                SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
+              ),
+              SizedBox(width: 13),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      child: Text(
                         '이다민',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(fontSize: 16),
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
+                    ),
+                    SizedBox(
+                      height: 13,
+                    ),
+                    Align(
+                      child: Text(
                         '안녕하세요. 저는 이런 사람입니다.:)',
                         overflow: TextOverflow.ellipsis,
                         style: TextStyle(fontSize: 16),
+                        maxLines: 5,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
+          Divider()
         ],
       ),
     );
